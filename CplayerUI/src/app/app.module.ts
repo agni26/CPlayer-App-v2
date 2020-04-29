a9e2d0c70a254c4b74505408b84bcb25656a4356
shivaagn <shivaagn@in.ibm.com>
2020-04-29 17:35:17 +0530

    Updated

--- CplayerUI/src/app/app.module.ts
+++ CplayerUI/src/app/app.module.ts
@@ -1,22 +1,74 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
-import {ReactiveFormsModule} from "@angular/forms";
 import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
+
+import { AppRoutingModule } from './app-routing.module';
+import {ReactiveFormsModule} from "@angular/forms";
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {MatToolbarModule} from '@angular/material/toolbar';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSelectModule } from '@angular/material/select';
+import { MatListModule } from '@angular/material/list'
+import { MatDialogModule } from '@angular/material/dialog'
+import { MatExpansionModule, matExpansionAnimations } from '@angular/material/expansion';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { MatCardModule } from '@angular/material/card';
+import { FormsModule } from '@angular/forms';
+import { RouterModule, Routes, ROUTES } from '@angular/router';
+import {NgxPaginationModule} from 'ngx-pagination';
+import {AngularFontAwesomeModule } from 'angular-font-awesome';
+import { MatTabsModule } from '@angular/material/tabs';
+import { SearchComponent } from './search/search.component';
+import { StatsComponent } from './stats/stats.component';
+import { FavsComponent } from './favs/favs.component';
+import { EdituserComponent } from './edituser/edituser.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { CalendarComponent } from './calendar/calendar.component';
+import { ReviewsComponent } from './reviews/reviews.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    FooterComponent
+    FooterComponent,
+    HeaderComponent,
+    SearchComponent,
+    StatsComponent,
+    FavsComponent,
+    EdituserComponent,
+    DashboardComponent,
+    CalendarComponent,
+    ReviewsComponent
   ],
   imports: [
     BrowserModule,
+    MatIconModule,
+    LayoutModule,
+    MatButtonModule,
+    MatDialogModule,
+    MatFormFieldModule,
+    MatInputModule,
+    HttpClientModule,
+    MatCardModule,
+    MatListModule,
+    MatExpansionModule,
+    MatSelectModule,
+    MatSidenavModule,
+    MatTabsModule,
+    AngularFontAwesomeModule,
+    NgxPaginationModule,
+    RouterModule,
+    FormsModule,
+
     AppRoutingModule,
     ReactiveFormsModule,
     BrowserAnimationsModule,
